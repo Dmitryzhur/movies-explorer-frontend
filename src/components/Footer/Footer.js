6f21a90dcdbdb9a3098b420bcf1e006c0ac84ba1
Dmitry Zhuravlev <dmitryzhur@mail.ru>
2022-11-02 22:10:20 +0500

    feat: add Footer and microfix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,18 @@
-function Footer() {
+import './Footer.css';
 
+function Footer() {
+	return (
+		<footer className="footer">
+			<h3 className='footer__title'>Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+			<div className='footer__info'>
+				<p className='footer__year'>&copy;2022</p>
+				<nav className='footer__nav'>
+					<a href='https://practicum.yandex.ru/web/' className='footer__link'>Яндекс.Практикум</a>
+					<a href='https://github.com/Dmitryzhur/' className='footer__link'>Github</a>
+				</nav>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer;
