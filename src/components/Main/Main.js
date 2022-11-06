87b71f2a5de521ff790348f676b402022847fcc0
Dmitry Zhuravlev <dmitryzhur@mail.ru>
2022-11-06 15:53:22 +0500

    feat: add Promo, NavTab

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Promo from	'../Promo/Promo';
 
 function Main() {
 	return (
 		<div>
 			<Header />
+			<Promo />
 			<Footer />
 		</div>
 	)
--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,5 +1,13 @@
-function NavTab() {
+import './NavTab.css';
 
+function NavTab() {
+ return (
+	<nav className="navtab">
+		<a href="#" className='navtab__link'>О проекте</a>
+		<a href="#" className='navtab__link'>Технологии</a>
+		<a href="#" className='navtab__link'>Студент</a>
+	</nav>
+ )
 }
 
 export default NavTab;
