2f4e45031a4e6e7cb98da0c5bd1ae1f8e8bb629b
Dmitry Zhuravlev <dmitryzhur@mail.ru>
2022-11-14 15:12:18 +0500

    fix: change after review 2. Add NavBurger

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import "./Navigation.css";
 import { Link } from "react-router-dom";
+import NavBurger from '../NavBurger/NavBurger';
 
 function Navigation({ loggedIn }) {
+
+	const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+
+	const onEditProfile = () => {
+		setIsEditProfilePopupOpen(!isEditProfilePopupOpen);
+	}
+
+
 	if (!loggedIn) {
 		return (
 			<nav className="navigation">
@@ -11,16 +21,25 @@ function Navigation({ loggedIn }) {
 		)
 	} else {
 		return (
-			<nav className="navigation">
-				<div className="nav-movies">
-					<Link to="/movies" className="nav__link nav__link_movies nav__link_type_active">Фильмы</Link>
-					<Link to="/saved-movies" className="nav__link nav__link_saved-movies">Сохраненные фильмы</Link>
-				</div>
-				<Link to="/profile" className="nav__link nav__link_profile">
-					<p className="nav__link_profile-text">Аккаунт</p>
-					<div className="nav__link_profile-avatar"></div>
-				</Link>
-			</nav>
+			<>
+				<NavBurger
+					onEditProfile={onEditProfile}
+					isOpen={isEditProfilePopupOpen}
+				/>
+				<nav className={`navigation__burger ${isEditProfilePopupOpen ? "" : "navigation__burger_inactive"}`}>
+					<div className="navigation__container">
+						<div className="nav-movies">
+							<Link to="/" className="nav__link nav__burger-link nav__link_main">Главная</Link>
+							<Link to="/movies" className="nav__link nav__burger-link nav__link_movies nav__link_type_active">Фильмы</Link>
+							<Link to="/saved-movies" className="nav__link nav__burger-link nav__link_saved-movies">Сохраненные фильмы</Link>
+						</div>
+						<Link to="/profile" className="nav__link nav__burger-link nav__link_profile">
+							<p className="nav__link_profile-text">Аккаунт</p>
+							<div className="nav__link_profile-avatar"></div>
+						</Link>
+					</div>
+				</nav>
+			</>
 		)
 	}
 }
